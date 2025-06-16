ccdad91c0475ec1b918ed55f28dcb89a63cc513d
John Arne Føreland <john.foreland@me.com>
2025-06-16 12:26:29 +0200

    ci: fix semantic (#12)
    * removed unused npm command
    
    * changed publish title
    
    * added more branch prefixes
    
    * adjusted when to build and make changelogs
    
    * remove presets
    
    * removed conventional-changelog-conventionalcommits

--- release.config.mjs
+++ release.config.mjs
@@ -7,29 +7,31 @@ export default {
     [
       '@semantic-release/commit-analyzer',
       {
-        preset: 'conventionalcommits',
         releaseRules: [
-          {
-            type: 'build',
-            scope: 'deps',
-            release: 'patch',
-          },
+          { type: 'breaking', release: 'major' },
+          { type: 'feat', release: 'minor' },
+          { type: 'fix', release: 'minor' },
+          { type: 'refactor', release: 'minor' },
+          { type: 'ci', release: 'patch' },
+          { type: 'build', release: 'patch' },
+          { type: 'dependabot', release: 'patch' },
         ],
       },
     ],
     [
       '@semantic-release/release-notes-generator',
       {
-        preset: 'conventionalcommits',
         presetConfig: {
           types: [
+            { type: 'breaking', section: 'Breaking Changes' },
             { type: 'feat', section: 'Features' },
             { type: 'fix', section: 'Bug Fixes' },
-            {
-              type: 'build',
-              section: 'Dependencies and Other Build Updates',
-              hidden: false,
-            },
+            { type: 'refactor', section: 'Refactoring' },
+            { type: 'test', section: 'Tests' },
+            { type: 'docs', section: 'Documentation' },
+            { type: 'ci', section: 'CI Updates' },
+            { type: 'build', section: 'Build Updates' },
+            { type: 'dependabot', section: 'Dependabot' },
           ],
         },
       },
