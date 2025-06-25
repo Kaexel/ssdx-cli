44df52e48c8de33f26f100a6f3e4f57e9053c962
John Arne Føreland <john.foreland@me.com>
2025-06-25 14:19:08 +0200

    improved create org parameters + added parameter for disabling notifications

--- src/cli/create/create.command.ts
+++ src/cli/create/create.command.ts
@@ -1,3 +1,4 @@
+import colors from 'colors/safe.js';
 import { Command } from 'commander';
 import CreateOptions from './dto/create-options.dto.js';
 import { createScratchOrg } from './steps/create_org.js';
@@ -19,17 +20,28 @@ export default class CreateCommand {
     this.program
       .command('create')
       .description('Create a Scratch org')
-      .option('-n, --scratch-org-name <string>', 'The alias to give the Scratch Org')
-      .option('-d, --duration-days <number>', 'The amount of days to keep the Scratch Org', '5')
-      .option('-c, --config-file <string>', 'The Scratch Org config file (see ssdx-config.json for default value)')
-      .option('-v, --target-dev-hub <string>', 'The alias or username of the dev hub org')
+
+      .optionsGroup('Parameters')
+      .option(`-n, --scratch-org-name ${colors.yellow('<string>')}`, 'The alias to give the Scratch Org')
+      .option(`-d, --duration-days ${colors.yellow('<number>')}`, 'The amount of days to keep the Scratch Org', '5')
+      .option(
+        `-c, --config-file ${colors.yellow('<string>')}`,
+        'The Scratch Org config file (see ssdx-config.json for default value)'
+      )
+      .option(`-v, --target-dev-hub ${colors.yellow('<string>')}`, 'The alias or username of the dev hub org')
+
+      .optionsGroup('Flags')
+      .option('--delete-current-org', 'Delete the current Scratch Org')
+      .option('--disable-notifications', 'Disabled OS notifications for steps')
       .option('--skip-dependencies', 'Skip dependency installation')
       .option('--skip-deployment', 'Skip deployment step')
+
       .action((options: CreateOptions) => {
         this.options = options;
         void this.main();
       });
   }
+
   private async main() {
     await initialize(this.options);
     await delete_question(this.options);
