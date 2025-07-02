ba3ace178397a7d3c80732e0f1a68673c8ccfd5b
John Arne Føreland <john.foreland@me.com>
2025-07-02 19:03:32 +0200

    remove success notification on scratch create

--- src/cli/create/steps/create_org.ts
+++ src/cli/create/steps/create_org.ts
@@ -71,7 +71,6 @@ class create_org {
       this.spinner.suffixText = `done! (${colors.yellow(CreateOptions.scratchOrgResult.username || '')})`;
       this.spinner.succeed();
       print.info(this.successText);
-      print.notificationSuccess(this.successText);
     } catch (error) {
       this.spinner.fail('Failed to create Scratch Org');
       throwError(String(error));
